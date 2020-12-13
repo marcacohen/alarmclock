62f25485191f05d8048d91e42e3a1c81dbba4ca1
marcacohen <marcacohen@gmail.com>
2020-12-13 17:38:25 +0000

    ignocnito and volume fix

--- src/script.ts
+++ src/script.ts
@@ -377,7 +377,7 @@ class Main {
 			this.fileReader = new FileReader();
 			this.fileReaderDone = true;
 			$("#player")[0].src = audio;
-			$("#player")[0].volume = .2;
+			$("#player")[0].volume = .1;
 			$(".alarm-melody").addClass("toggled");
 		}
 
