6fb653afc77b33216e76392bd56593c08132f855
marcacohen <marcacohen@gmail.com>
2021-01-10 18:13:39 +0000

    fix wrapper syntax

--- docker/wrapper.sh
+++ docker/wrapper.sh
@@ -1,3 +1,1 @@
-/usr/bin/chromium-browser --user-agent='Mozilla/5.0 (X11; CrOS armv7l 12607.82.0) AppleWebKit/537.36
- (KHTML, like Gecko) Chrome/79.0.3945.123 Safari/537.36' --start-fullscreen /home/pi/p/alarmclock/sr
-c/index.html
+/usr/bin/chromium-browser --user-agent='Mozilla/5.0 (X11; CrOS armv7l 12607.82.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.123 Safari/537.36' --start-fullscreen /home/pi/p/alarmclock/src/index.html
