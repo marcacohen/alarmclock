ee664a38d8be6df94eab144311c8735bb91ae91c
marcacohen <marcacohen@gmail.com>
2021-01-10 17:52:58 +0000

    remove unused js files

--- src/player2.js
+++ src/player2.js
@@ -3854,8 +3854,7 @@
                                         if (document.getElementById("spotify-player")) e();
                                         else {
                                             var n = document.createElement("script");
-                                            //n.id = "spotify-player", n.type = "text/javascript", n.async = !1, n.defer = !0, n.src = "https://sdk.scdn.co/spotify-player.js", n.onload = function() {
-                                            n.id = "spotify-player", n.type = "text/javascript", n.async = !1, n.defer = !0, n.src = "http://localhost:8000/spotify-player.js", n.onload = function() {
+                                            n.id = "spotify-player", n.type = "text/javascript", n.async = !1, n.defer = !0, n.src = "https://sdk.scdn.co/spotify-player.js", n.onload = function() {
                                                 return e()
                                             }, n.onerror = function(e) {
                                                 return t(new Error("loadScript: " + e.message))
