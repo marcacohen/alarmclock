04a5d7d356dedfe3ede6b1390ef4f5962bad7702
marcacohen <marcacohen@gmail.com>
2021-01-14 14:20:46 +0000

    fix paths

--- src/updater.sh
+++ src/updater.sh
@@ -1,9 +1,14 @@
 cd $HOME/p/alarmclock
 git fetch
 git reset --hard
+docker/bld
 
-#autostart
-#crontab
-#pi.desktop
-#docker/bld
-#docker/chromium-*
+exit 0
+
+FILES="autostart: pi.desktop: docker/chromium-*:"
+for i in FILES
+do
+  if $(diff $i  >/dev/null); then echo same; else echo diff; fi
+done
+
+crontab
